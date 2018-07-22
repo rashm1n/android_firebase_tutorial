6ed1bba04594fc8b86bac1b9a0172aa2111d9b0b
ravindurashmin <rashmin.ravindu@gmail.com>
2018-07-22 13:18:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rashminpc/android_firebase/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rashminpc.android_firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
