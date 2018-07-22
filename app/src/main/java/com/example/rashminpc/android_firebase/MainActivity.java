71dc369140336ac62a23181f4d5520432cb12270
ravindurashmin <rashmin.ravindu@gmail.com>
2018-07-22 16:14:11 +0530

    completed app

--- app/src/main/java/com/example/rashminpc/android_firebase/MainActivity.java
+++ app/src/main/java/com/example/rashminpc/android_firebase/MainActivity.java
@@ -2,12 +2,80 @@ package com.example.rashminpc.android_firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+    final DatabaseReference ledstatus1 = myRef.child("led1").child("status");
+
+    final DatabaseReference ledstatus2 = myRef.child("led2").child("status");
+
+    final DatabaseReference ledstatus3 = myRef.child("led3").child("status");
+
+
+
+    Button b1;
+    Button b2;
+    TextView textView1;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button1);
+        b2 = (Button)findViewById(R.id.button2);
+
+        textView1 = (TextView)findViewById(R.id.textView1);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        //updateText(ledstatus1,textView1);
+
+        ledstatus1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("file", "Value is: " + value);
+                textView1.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ledstatus1.setValue("ON");
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ledstatus1.setValue("OFF");
+            }
+        });
+
+
+
     }
 }
